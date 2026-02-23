6405583647508f43efe4fb15e7b6c8888dc2fea6
karanpratap <kpsshishodia123@gmail.com>
2026-02-23 11:20:26 +0530

    renamed

---
+++ Seassion-2/Episode - 5(Middleware , Routing, Error Handlers)/src/middleware/Auth.js
@@ -0,0 +1,35 @@
+const AdminAuth = (req,res,next) => {
+    console.log("Admin Auth getting checked.")
+    const token = "xyz"
+    const isAdminAuthorized = token === "xyz"
+    if(!isAdminAuthorized){
+     res.status(401).send("Unauthorized request.")
+    }else{
+     next();
+    }
+
+}
+
+const UserAuth = (req,res,next) => {
+    console.log("User Auth getting checked.")
+    const token = "xyz"
+    const isUserAuthorized = token === "xyz"
+    if(!isUserAuthorized){
+     res.status(401).send("Unauthorized request.")
+    }else{
+     next();
+    }
+
+}
+
+module.exports = {
+AdminAuth,
+UserAuth
+}
+
+
+
+
+
+
+
