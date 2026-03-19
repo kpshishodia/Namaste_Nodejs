65aa44a930cd89a872cadfe2257f7ccf6094e605
karanpratap <kpsshishodia123@gmail.com>
2026-03-20 01:17:36 +0530

    add hospital models

--- Seassion-2/Episode-10(Express Router,Data Modeling)/src/models/hospital_management/doctor.models.js
+++ Seassion-2/Episode-10(Express Router,Data Modeling)/src/models/hospital_management/doctor.models.js
@@ -4,6 +4,34 @@ const {Schema} = mongoose
 
 const doctorSchema = new Schema({
 
+name : {
+    type : String,
+    required : true
+},
+
+salary : {
+    type : Number,
+    required : true
+},
+
+qualification : {
+    type : String,
+    required : true
+},
+
+experience : {
+    type : String,
+    required : true,
+    default : 0
+},
+
+worksInHospitals : [
+    {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Hospital"
+    }
+]
+
 },{timestamps : true})
 
 const Doctor = mongoose.model("Doctor" , doctorSchema , Doctor)
--- Seassion-2/Episode-10(Express Router,Data Modeling)/src/models/hospital_management/hospital.models.js
+++ Seassion-2/Episode-10(Express Router,Data Modeling)/src/models/hospital_management/hospital.models.js
@@ -4,6 +4,35 @@ const {Schema} = mongoose
 
 const hospitalSchema = new Schema({
 
+    name : {
+    type : String,
+    required : true
+},
+
+address : {
+    type : String,
+    required : true
+},
+
+city : {
+    type : String,
+    required : true
+},
+
+specializedIn : [
+    {
+    type : String,
+    required : true
+}
+],
+
+reviews : [
+    {
+    type : String,
+    required : true
+}
+]
+
 } , {timestamps : true})
 
 const Hospital = mongoose.model("Hospital" , hospitalSchema , Hospital)
