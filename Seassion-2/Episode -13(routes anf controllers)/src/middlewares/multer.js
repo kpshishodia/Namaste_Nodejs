9ad707616a6f60707cd9814c9e9240158324ac52
karanpratap <kpsshishodia123@gmail.com>
2026-04-09 16:44:12 +0530

    renamed multer  upload middleware in to fileUpload

--- Seassion-2/Episode -13(routes anf controllers)/src/middlewares/multer.js
+++ Seassion-2/Episode -13(routes anf controllers)/src/middlewares/multer.js
@@ -11,6 +11,6 @@ const storage = multer.diskStorage({
   }
 })
 
-const upload = multer({ storage: storage })
-
-module.exports = {upload}
+const fileUpload = multer({ storage: storage })
+ 
+module.exports = fileUpload
