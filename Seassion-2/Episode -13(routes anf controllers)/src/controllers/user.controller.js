193d70c5c5124650fc3fb23be44484a681e63921
karanpratap <kpsshishodia123@gmail.com>
2026-04-09 16:54:22 +0530

    renamed multer upload middleware in to fileUpload and import in user.route.js

--- Seassion-2/Episode -13(routes anf controllers)/src/controllers/user.controller.js
+++ Seassion-2/Episode -13(routes anf controllers)/src/controllers/user.controller.js
@@ -1,8 +1,39 @@
+// const registerUserController = async (req, res) => {
+//   try {
+//     return res.status(201).json({
+//       message: "okk",
+//     });
+//   } catch (error) {
+//     return res.status(400).json({
+//       message: "Bad request",
+//       error: error.message,
+//     });
+//   }
+// };
+
+// module.exports = registerUserController ;
+
+
 const registerUserController = async (req, res) => {
   try {
-    return res.status(201).json({
-      message: "okk",
-    });
+   
+    // steps to register user 
+
+// 1 . get user deatil from fronend (postman)
+// 2 . validations -- not empty -- require fields user can only send data which pass validation such as isEmail , isStrongPassword , etc.
+// 3 . check if user already exist -- return user already exist response
+// 4 . check for images , check for avatar
+// 5 . upload them to cloudinary service
+// 6 . extract url from cloudinay service response
+// 7 . create user object == create entry in DB
+// 8 . remove password and refresh token field from response
+// 9 . check for user creation 
+// 10 . return response
+
+const {firstName,lastName,email,password} = req.body
+console.log("email:" , email , "firstName:" , firstName)
+res.status(201).send("success")
+
   } catch (error) {
     return res.status(400).json({
       message: "Bad request",
--- Seassion-2/Episode -13(routes anf controllers)/src/routes/user.route.js
+++ Seassion-2/Episode -13(routes anf controllers)/src/routes/user.route.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const registerUserController = require("../controllers/user.controller.js");
 const userRouter = express.Router();
+const fileUpload = require("../middlewares/multer.js")
 
-userRouter.route("/register").post(registerUserController)
+userRouter.route("/register").post(
+    fileUpload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 3        
+        }
+    ]),
+    registerUserController)
 
 module.exports = userRouter;
