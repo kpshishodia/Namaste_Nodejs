23c0044bf5b02c22b0610e979f6c1014b49f3e71
karanpratap <kpsshishodia123@gmail.com>
2026-06-03 23:58:52 +0530

    update

--- Seassion-2/Music player/src/controllers/Profile/updateProfile.controller.js
+++ Seassion-2/Music player/src/controllers/Profile/updateProfile.controller.js
@@ -4,7 +4,21 @@ import User from "../../models/user.model";
 const updateProfileController = async (req , res) =>{
 
      try {
-   
+   // 1. get email  , userName , about  fields user want to update from req.body
+
+   const {userName , about , email} = req.body
+
+  //  if(!email || !userName || !about){
+  //   return res.status(400).json({
+  //       message: "All fields are required",
+  //       error : error.message
+  //   })
+
+    // 2. get user._id from verifyJWT middleware
+
+    
+
+   }
 
   } catch (error) {
     // ----------------------------------------------------
